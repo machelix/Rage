051a19ef7a9173a71898c18fb9be36f3963ed79e
Rajesh Huria <rajesh.huria@gmail.com>
2014-01-07 21:03:18 +0100

    admin pwd updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  http_basic_authenticate_with name: "admin", password: "thankyougeorg" # except: :index
+  http_basic_authenticate_with name: "admin", password: "alphabetagamma" # except: :index
 
 
   def index
